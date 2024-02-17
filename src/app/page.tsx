f3b15927097b12bc8aea0b76d6ca0e0fef09210d
Aline Areda <aredaaline@gmail.com>
2024-02-17 13:34:09 -0300

    ajuste bg-white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import heroImg from '@/assets/hero.svg'
 
 export default function Home() {
   return (
-    <main className="flex items-center flex-col justify-center min-h-[calc(100vh-80px)]">
+    <main className="flex items-center flex-col justify-center min-h-[calc(100vh-80px)] bg-white">
       <h2 className="font-medium text-2xl mb-2">Canal de suporte</h2>
       <h1 className="font-bold text-3xl mb-8 text-blue-950 md:text-4xl">Atendimentos, alunos</h1>
       <Image
