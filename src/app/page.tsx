aedf2fec7c23b8ac17a7b54c95bb14cca8290ed5
Aline Areda <aredaaline@gmail.com>
2024-03-18 19:47:59 -0300

    Correção titulo da home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex items-center flex-col  bg-white justify-center min-h-[calc(100vh-80px)]">
       <h2 className="font-medium text-2xl mb-2">Canal de suporte</h2>
-      <h1 className="font-bold text-3xl mb-8 text-blue-950 md:text-4xl">Atendimemtos, alunos</h1>  
+      <h1 className="font-bold text-3xl mb-8 text-blue-950 md:text-4xl">Atendimentos, alunos</h1>  
       <Image
         src={heroImg}
         alt="Imagem hero do dev controle"
